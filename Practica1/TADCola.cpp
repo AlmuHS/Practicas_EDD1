e05621b64c298797fb8ea0373d3182271a8135fe
AlmuHS <liberamenso10000@gmail.com>
2017-07-31 19:32:30 +0200

    Anadidas licencias a ficheros Practica1

--- Practica1/TADCola.cpp
+++ Practica1/TADCola.cpp
@@ -1,3 +1,20 @@
+/* Copyright 2013 Almudena García Jurado-Centurión
+    This file is part of P1
+
+    P1 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    P1 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with P1.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "TADCola.h"
 
 cola::cola() {
