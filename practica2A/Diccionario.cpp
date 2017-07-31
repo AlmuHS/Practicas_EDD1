66945cf0e1159732b002d6158c037a5dd321eb9c
AlmuHS <liberamenso10000@gmail.com>
2017-07-31 19:28:48 +0200

    Anadidas licencias a ficheros Practica2A

--- practica2A/Diccionario.cpp
+++ practica2A/Diccionario.cpp
@@ -1,3 +1,20 @@
+/* Copyright 2013 Almudena García Jurado-Centurión
+    This file is part of Practica2A
+
+    Practica2A is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Practica2A is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Practica2_AMC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "Diccionario.h"
 
 bool diccionario::abrirdiccionario(cadena2 ruta){
